628ba0f590e8f97404bfb85c466d42fa2e48a0b9
Jo Emil <jo.emil@skriveleif.com>
2016-04-09 12:09:05 +0200

    Release 1.0 Beta onto Master

---
+++ frontend/src/js/leet.js
@@ -0,0 +1,110 @@
+$(document).ready(function(){
+    GetData();
+});
+
+var data;
+
+function GetData(day)
+{
+    var api = "http://thorium.skriveleif.com:9200/irc-leet/_search";
+
+    var date = new Date();
+    if (day)
+    {
+        date = new Date(day.id);
+
+        $('.dropdown-toggle').text($(day).text());
+        $('.dropdown-toggle').append("<b class='caret'></b>");
+    }
+    date.setMilliseconds(0);
+    date.setSeconds(0);
+    date.setMinutes(30);
+    date.setHours(13);
+
+    $.ajax(api, {
+        data: JSON.stringify({
+            "fields": [
+                "time",
+                "status",
+                "nick",
+                "msg"
+            ],
+            "query": {
+                "filtered": {
+                    "query": {
+                        "bool": {
+                            "must": [
+                                {
+                                    "terms": {
+                                        "_type": [
+                                            "0",
+                                            "3",
+                                            "5",
+                                            "6"
+                                        ]
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    "filter": {
+                        "range": {
+                            "@timestamp": {
+                                "gte": date.getTime(),
+                                "lte": date.getTime(date.setMinutes(45))
+                            }
+                        }
+                    }
+                }
+            },
+            "size": 500,
+            "sort": [
+                {
+                    "@timestamp": {
+                        "order": "asc"
+                    }
+                }
+            ]
+        }),
+        success: function(_data)
+        {
+            data = _data.hits;
+            AddScore(data);
+        },
+        type: 'POST'
+    });
+}
+
+function AddScore(d)
+{
+    $('.list-group').empty();
+    if (d.total > 0)
+    {
+        $.each(d.hits, function(index){
+            var date = new Date(this.sort[0]);
+
+            var c = (date.getMinutes() == 37) ? "alert-success" : "alert-danger";
+            if (this._type == 3) c = "alert-danger";
+
+            var str = addZ(date.getSeconds()) +"."+ addZ2(date.getMilliseconds());
+
+            if (date.getMinutes() != 37)
+            {
+                str = (date.getMinutes() <= 36 ? "- ":"+ ") + date.getMinutes()+"."+str;
+
+                var x = date.getMinutes() +"."+ date.getSeconds();
+                if (x != 36.59 && x != 38.00) str = str.substr(0, str.length - 4);
+            }
+
+            var f = "<li class='list-group-item'> <span class='badge "+c+"'>"+
+                str
+                +"</span>"+this.fields.nick[0]+"</li>";
+            $('.list-group').append(f);
+
+        });
+    } else {
+        $('#ErrorModalLabel').text('No leet for you!');
+        $('.modal-body').text('Please select another day!');
+        $('#ErrorModal').modal();
+    }
+}
